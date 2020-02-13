815bc7383dd340817c4af90c6df7fa3bb96f5028
Kaiyun Fu <kaf011@ucsd.edu>
2020-02-12 22:25:43 -0800

    data.json

--- routes/app.js
+++ routes/app.js
@@ -9,17 +9,9 @@ var path = require('path');
 var handlebars = require('express3-handlebars');
 
 var index = require('./routes/index');
-var home = require('./routes/home');
-var profile = require('./routes/profile');
-var profile_records = require('./routes/profile_records');
-var signup = require('./routes/signup');
-var step1 = require('./routes/step1');
-var step2 = require('./routes/step2');
-var step3 = require('./routes/step3');
-var forget_your_password = require('./routes/forget_your_password');
-var finish = require('./routes/finish');
+var hello = require('./routes/hello');
 
-//var project = require('./routes/project');
+var project = require('./routes/project');
 
 // Example route
 // var user = require('./routes/user');
@@ -46,18 +38,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Add routes here
 app.get('/', index.view);
-app.get('/home', home.view);
-app.get('/profile', profile.viewProject);
-app.get('/profile_records', profile_records.viewProject);
-app.get('/step1', step1.viewProject);
-app.get('/step2', step2.viewProject);
-app.get('/step3', step3.viewProject);
-app.get('/signup', signup.viewProject);
-app.get('/forget_your_password', forget_your_password.viewProject);
-app.get('/finish', finish.viewProject);
-//app.get('/add', add.addRecord);
-
+app.get('/hello/:userName', hello.view);
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
