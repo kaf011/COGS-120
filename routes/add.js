c0beb93c4e376f20a84abfc4a4efaadd692d1cf8
cleoxiao <yax064@ucsd.edu>
2020-02-12 22:59:35 -0800

    finish add.js

--- routes/add.js
+++ routes/add.js
@@ -2,14 +2,14 @@ var data = require("../data.json");
 
 exports.addRecord = function(request, response) {  
     var d = new Date();
-	var newDate = d.getDate();
-    var newTime = d.getTime();
+	var newDate = d.getFullYear()+"/"+(d.getMonth()+1)+"/"+d.getDate();
+    var newTime = d.getHours()+":"+d.getMinutes();
     var rate = "fas fa-star-half-alt";
 	var newRecord = {
 	 "Date": newDate,
 	 "Time": newTime,
 	 "Health": rate,
 	};
-	data.friends.push(newRecord);
+	data.records.push(newRecord);
 	response.render('profile_records', data);
 }	
