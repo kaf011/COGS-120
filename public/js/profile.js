42855cdce50cfda1ca7dcdf83afba5cecb82a9bb
Nora Fu <noorafuu@gmail.com>
2020-02-13 18:17:40 -0800

    fixed profile page showing the first 4 records

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
 	initializePage();
 	//$('#profile_record').hide();
-	$('#profile_record tr:gt(4)').remove();
+	$('ul li:gt(3)').hide();
 })
 
 /*
@@ -13,7 +13,8 @@ $(document).ready(function() {
 function initializePage() {
 
 	// example: $("#div-id").click(functionToCall);
-	$(".record-btn").click(projectClick);
+	//$(".record-btn").click(addProjectDetails); //TODO
+	$(".record-btn").click(projectClick); //
 
 }
 
@@ -25,10 +26,11 @@ function projectClick(e) {
     // the object that triggered the event 
     //$(this).css("background-color", "#7fff00");
 
-	  //var containingProject = $(this).closest(".record");
+	  var record = $(this).closest(".record");
+		var id = record.attr('id');
+		var date = record.attr('Date');
+		var time = record.attr('Time');
 		var detail = $(this).find(".record-detail");
-		var date = $(this).find(".date").text;
-		var time = $(this).find(".time").text;
 		if (detail.length == 0){
 			var string = `<div class="record-detail"><h2>Meal Record</h2><p>Date: ${date}</p><p>Time: ${time}</p><p>Meal Ends: </p><p>Durition: </p><p>Health Rate: </p>`;
 			$(this).append(string);
@@ -42,3 +44,72 @@ function projectClick(e) {
     }
 
 }
+
+ // TODO: function to use later, new files required.
+function addProjectDetails(e) {
+	// Prevent following the link
+	e.preventDefault();
+
+	// Get the div ID, e.g., "project3"
+	var projectID = $(this).closest('.record').attr('id');
+	// get rid of 'project' from the front of the id 'project3'
+	var idNumber = projectID.substr('record'.length);
+  $.get("/record/"+idNumber, callBackFn);
+
+	console.log("User clicked on record " + idNumber);
+
+	console.log("URL: " + "/record/"+idNumber);
+}
+
+function callBackFn(result){
+  console.log(result);
+	var projectHTML = "";
+  console.log(projectHTML);
+	$("#record"+result["id"]+" .details").html(projectHTML);
+}
+
+// // index.handlebars
+// {{#each projects}}
+// <div class="project" id="{{id}}">
+// 	<div class="thumbnail">
+// 		<a href="#">
+// 			<img src="images/{{image}}" alt="Lorem Pixel image" class="img">
+// 			<p>{{name}}</p>
+// 		</a>
+// 		<div class="details"></div>
+// 	</div>
+// </div>
+// {{/each}}
+//
+//
+// // introHCI.js
+// function initializePage() {
+// 	$('.project a').click(addProjectDetails);
+//
+// 	// $('#colorBtn').click(randomizeColors);
+// }
+//
+// /*
+//  * Make an AJAX call to retrieve project details and add it in
+//  */
+// function addProjectDetails(e) {
+// 	// Prevent following the link
+// 	e.preventDefault();
+//
+// 	// Get the div ID, e.g., "project3"
+// 	var projectID = $(this).closest('.project').attr('id');
+// 	// get rid of 'project' from the front of the id 'project3'
+// 	var idNumber = projectID.substr('project'.length);
+//   $.get("/project/"+idNumber, callBackFn);
+//
+// 	console.log("User clicked on project " + idNumber);
+//
+// 	console.log("URL: " + "/project/"+idNumber);
+// }
+//
+// function callBackFn(result){
+//   console.log(result);
+// 	var projectHTML = '<img src="' + result["image"] + '" class="detailsImage">' + "<h5>" + result["title"] + "</h5> <h6>" + result["date"] + "</h6> <p>" + result["summary"] + "</p>";
+//   console.log(projectHTML);
+// 	$("#project"+result["id"]+" .details").html(projectHTML);
+// }
