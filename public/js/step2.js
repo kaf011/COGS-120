09bae89c17c04e4635f2d34c2b5ace7dfd2acfe0
Kaiyun Fu <kaf011@ucsd.edu>
2020-02-21 15:18:39 -0800

    choose food page

--- public/js/step2.js
+++ public/js/step2.js
@@ -1,7 +1,25 @@
-.food - btn - chosen
-
 $(document).ready(function() {
   initializePage();
   //$('#profile_record').hide();
-
+  $(".food-btn-chosen").click(projectClick);
 })
+
+
+function projectClick(e) {
+  // prevent the page from reloading
+
+  e.preventDefault();
+  // In an event handler, $(this) refers to
+  //var chosen = $(this).find(".food-btn-chosen-chosen");
+  // if (chosen.length == 0) {
+  //
+  //   $(this).addClass("food-btn-chosen-chosen");
+  //
+  // } else {
+  $(this).toggleClass("food-btn-chosen-chosen");
+  //
+  // }
+
+
+
+}
