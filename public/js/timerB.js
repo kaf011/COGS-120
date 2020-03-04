1d20ca6d92223acefa5cd7962d626ca750dc4b4c
Kaiyun Fu <kaf011@ucsd.edu>
2020-03-04 02:33:56 -0800

    hii

--- public/js/timerB.js
+++ public/js/timerB.js
@@ -82,15 +82,30 @@ const secondsDiv = document.getElementById('seconds').children[0];
 
 // set the time
 set.onclick = () => {
-  ga('send', 'event', 'Timer', 'set');
-  if (window.performance) {
-    // Gets the number of milliseconds since page load
-    // (and rounds the result since the value must be an integer).
-    var timeSincePageLoad = Math.round(performance.now());
-
-    // Sends the timing hit to Google Analytics.
-    ga('send', 'timing', 'JS Dependencies', 'load', timeSincePageLoad);
-  }
+  var timeSincePageLoad = Math.round(performance.now());
+  console.log(timeSincePageLoad);
+  //ga('send', 'event', 'Timer', 'set');
+  // ga('send', {
+  //   hitType: 'timing',
+  //   timingCategory: 'JS Dependencies',
+  //   timingVar: 'load',
+  //   timingValue: timeSincePageLoad
+  // });
+  ga('send', {
+    hitType: 'timing',
+    timingCategory: 'JS Dependencies',
+    timingVar: 'load',
+    timingValue: 3549
+  });
+  // ga('send', 'event', 'Timer', 'set');
+  // if (window.performance) {
+  //   // Gets the number of milliseconds since page load
+  //   // (and rounds the result since the value must be an integer).
+  //   var timeSincePageLoad = Math.round(performance.now());
+  //
+  //   // Sends the timing hit to Google Analytics.
+  //   ga('send', 'timing', 'JS Dependencies', 'load', timeSincePageLoad);
+  // }
   // ga('send', 'event', 'Timer', 'set');
   // ga('send', 'timing', 'Timer', 'set', timeSincePageLoad);
   document.getElementById('minutes').style.display = 'inline-block';
